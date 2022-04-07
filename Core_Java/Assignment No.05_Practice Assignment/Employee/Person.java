a30017d53b77761867668656db5426d9211979c2
ShrutikaJantre <81174919+ShrutikaJantre@users.noreply.github.com>
2022-04-07 19:31:07 +0530

    Delete Core_Java/Assignment No.05_Practice Assignment directory

--- Core_Java/Assignment No.05_Practice Assignment/Employee/Person.java
+++
@@ -1,9 +0,0 @@
-//Create a class called Person with a member variable name. Save it in a file called Person.java 
-class Person{
-	String name;
-	
-	Person(String name){
-		this.name = name;
-	}
-	
-}
